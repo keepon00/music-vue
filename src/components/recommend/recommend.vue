10124bb6593c185d76e9e33b49ebd3ac8e3797a3
oo <1142908096@qq.com>
2018-03-05 17:58:09 +0800

    vuelazyload 图片懒加载

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,7 @@
           <slider>
             <div v-for="(item, index) in recommends" :key="index">
               <a :href="item.linkUrl">
-                <img @load="loadImage" :src="item.picUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="(item, index) in discLists" :key="index" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl">
+                <img width="60" height="60" v-lazy="item.imgurl">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -48,10 +48,7 @@
       }
     },
     created() {
-      setTimeout(() => {
-        this._getRecommend()
-      }, 2000)
-
+      this._getRecommend()
       this._getDiscList()
     },
     methods: {
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import vueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
+
+Vue.use(vueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
 fastclick.attach(document.body)
 // Vue.config.productionTip = false
 
